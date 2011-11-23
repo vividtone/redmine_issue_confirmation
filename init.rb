0a74e70b985f069bae769cac7309be70b11edeee
MAEDA Go <maeda@farend.jp>
2011-11-23 15:54:39 +0900

    generated plugin template.

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_issue_confirmation do
+  name 'Redmine Issue Confirmation plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
